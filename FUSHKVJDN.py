96909c541595afc90af090f8957bf146ae48695b
HGreender <timurkostyan@mail.ru>
2024-08-14 15:58:25 +0300

    Add counter in MyTest class

--- FUSHKVJDN.py
+++ FUSHKVJDN.py
@@ -1,13 +1,24 @@
 from  Tetsik import *
 
 class MyTest(Test):
+    _counter = 0
     def __init__(self):
         super().__init__()
         self.prit = 'PrInT'
+
+        MyTest._counter += 1
+        self.id = MyTest._counter
 
     def prt(self):
         print(self.prit)
 
 
-cls = MyTest()
-cls.prt()
+cls1 = MyTest()
+cls1.prt()
+print(cls1.id)
+
+cls2 = MyTest()
+print(cls2.id)
+
+cls3 = MyTest()
+print(cls3.id)
